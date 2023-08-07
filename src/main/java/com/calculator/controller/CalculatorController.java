7117339d42b48fec65e99a111851c4d8667919d2
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-08-07 09:47:08 +0530

    implemented log4j

--- src/main/java/com/calculator/controller/CalculatorController.java
+++ src/main/java/com/calculator/controller/CalculatorController.java
@@ -1,10 +1,16 @@
 package com.calculator.controller;
 
+import com.calculator.CalculatorApplication;
 import com.calculator.model.CalculatedResponse;
 import com.calculator.model.CalculatorRequest;
 import com.calculator.service.CalculatorService;
 import jakarta.validation.Valid;
+import lombok.extern.java.Log;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +19,10 @@ import org.springframework.web.bind.annotation.*;
  * A simple arithmetic calculator supporting addition, subtraction, multiplication, and division for multiple numbers.
  */
 @RestController
-public class CalculatorController {
+public class CalculatorController implements ApplicationRunner {
+
+    private static final Logger logger = LogManager.getLogger(CalculatorController.class);
+
     @Autowired
     private CalculatorService service;
 
@@ -41,4 +50,12 @@ public class CalculatorController {
         return ResponseEntity.status(HttpStatus.OK).body(calculatedResponse);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        logger.debug("Debugging log");
+        logger.info("Info log");
+        logger.warn("Hey, This is a warning!");
+        logger.error("Oops! We have an Error. OK");
+        logger.fatal("Damn! Fatal error. Please fix me.");
+    }
 }
